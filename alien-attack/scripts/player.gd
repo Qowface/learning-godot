3cdc9d3d62973cdb6ef0229ca65843821f4f0e3d
Robert Marquess <qowsrule@gmail.com>
2024-01-19 22:23:38 -0500

    Alien Attack - Implement player movement.

--- alien-attack/scripts/player.gd
+++ alien-attack/scripts/player.gd
@@ -1,1 +1,26 @@
 extends CharacterBody2D
+
+
+@export var speed = 300
+var screen_size
+
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	
+	move_and_slide()
+	
+	global_position = global_position.clamp(Vector2.ZERO, screen_size)
