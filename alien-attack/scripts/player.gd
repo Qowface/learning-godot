2c9c8972f1333c2ca639334319acf90635288e67
Robert Marquess <qowsrule@gmail.com>
2024-01-20 19:53:10 -0500

    Alien Attack - Implement shooting rockets.

--- alien-attack/scripts/player.gd
+++ alien-attack/scripts/player.gd
@@ -4,9 +4,18 @@ extends CharacterBody2D
 @export var speed = 300
 var screen_size
 
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
+@onready var rocket_container = $RocketContainer
+
 
 func _ready():
 	screen_size = get_viewport_rect().size
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 
 func _physics_process(delta):
@@ -24,3 +33,10 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	global_position = global_position.clamp(Vector2.ZERO, screen_size)
+
+
+func shoot():
+	var rocket_instance = rocket_scene.instantiate()
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
+	rocket_instance.global_position.x += 80
