05f785f4c9c70a3f3a5c68c1741529243cb8b239
Robert Marquess <qowsrule@gmail.com>
2024-01-25 22:11:36 -0500

    Alien Attack - Add UI to display score and lives.

--- alien-attack/scripts/game.gd
+++ alien-attack/scripts/game.gd
@@ -5,6 +5,12 @@ var lives = 3
 var score = 0
 
 @onready var player = $Player
+@onready var hud = $UI/HUD
+
+
+func _ready():
+	hud.set_score_label(score)
+	hud.set_lives(lives)
 
 
 func _on_deathzone_area_entered(area):
@@ -13,11 +19,10 @@ func _on_deathzone_area_entered(area):
 
 func _on_player_took_damage():
 	lives -= 1
+	hud.set_lives(lives)
 	if lives == 0:
 		print("Game Over")
 		player.die()
-	else:
-		print(lives)
 
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
@@ -27,4 +32,4 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance):
 
 func _on_enemy_died():
 	score += 100
-	print("Score: " + str(score))
+	hud.set_score_label(score)
