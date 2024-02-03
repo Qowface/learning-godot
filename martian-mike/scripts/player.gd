7f9beb21c6877bf368c20b73982887f27adae6c0
Robert Marquess <qowsrule@gmail.com>
2024-02-02 22:39:22 -0500

    Martian Mike - Add jump pad.

--- martian-mike/scripts/player.gd
+++ martian-mike/scripts/player.gd
@@ -1,3 +1,4 @@
+class_name Player
 extends CharacterBody2D
 
 
@@ -15,7 +16,7 @@ func _physics_process(delta):
 			velocity.y = 500
 	
 	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y = -jump_force
+		jump(jump_force)
 	
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction != 0:
@@ -25,6 +26,10 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	update_animations(direction)
+
+
+func jump(force):
+	velocity.y = -force
 
 
 func update_animations(direction):
