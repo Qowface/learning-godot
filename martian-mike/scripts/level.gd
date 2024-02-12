b77d9b276beb5724f014f4f742228c216a9cb5f2
Robert Marquess <qowsrule@gmail.com>
2024-02-11 20:49:09 -0500

    Martian Mike - Add HUD to display level timer.

--- martian-mike/scripts/level.gd
+++ martian-mike/scripts/level.gd
@@ -7,6 +7,7 @@ extends Node2D
 @onready var start = $Start
 @onready var exit = $Exit
 @onready var death_zone = $Deathzone
+@onready var hud = $UILayer/HUD
 
 var player = null
 var timer_node = null
@@ -27,6 +28,7 @@ func _ready():
 	death_zone.body_entered.connect(_on_deathzone_body_entered)
 	
 	time_left = level_time
+	hud.set_time_label(time_left)
 	
 	timer_node = Timer.new()
 	timer_node.name = "Level Timer"
@@ -39,10 +41,11 @@ func _ready():
 func _on_level_timer_timeout():
 	if !win:
 		time_left -= 1
-		print(time_left)
+		hud.set_time_label(time_left)
 		if time_left < 0:
 			reset_player()
 			time_left = level_time
+			hud.set_time_label(time_left)
 
 
 func _process(delta):
