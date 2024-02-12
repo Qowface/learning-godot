99852cbf70a8dd53f778759e5256101afae5f830
Robert Marquess <qowsrule@gmail.com>
2024-02-11 20:36:32 -0500

    Martian Mike - Implement level timer.

--- martian-mike/scripts/level.gd
+++ martian-mike/scripts/level.gd
@@ -2,12 +2,16 @@ extends Node2D
 
 
 @export var next_level: PackedScene = null
+@export var level_time = 5
 
 @onready var start = $Start
 @onready var exit = $Exit
 @onready var death_zone = $Deathzone
 
 var player = null
+var timer_node = null
+var time_left
+var win = false
 
 
 func _ready():
@@ -21,6 +25,24 @@ func _ready():
 	
 	exit.body_entered.connect(_on_exit_body_entered)
 	death_zone.body_entered.connect(_on_deathzone_body_entered)
+	
+	time_left = level_time
+	
+	timer_node = Timer.new()
+	timer_node.name = "Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node)
+	timer_node.start()
+
+
+func _on_level_timer_timeout():
+	if !win:
+		time_left -= 1
+		print(time_left)
+		if time_left < 0:
+			reset_player()
+			time_left = level_time
 
 
 func _process(delta):
@@ -48,5 +70,6 @@ func _on_exit_body_entered(body):
 		if next_level != null:
 			exit.animate()
 			player.active = false
+			win = true
 			await get_tree().create_timer(1.5).timeout
 			get_tree().change_scene_to_packed(next_level)
