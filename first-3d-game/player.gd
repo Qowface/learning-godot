ec4ea2779186fe5bc1b8d351d0fe275e665c48d7
Robert Marquess <qowsrule@gmail.com>
2024-03-09 22:40:10 -0500

    First 3D Game - Implement jumping and squashing mobs.

--- first-3d-game/player.gd
+++ first-3d-game/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 
 @export var speed = 14
 @export var fall_acceleration = 75
+@export var jump_impulse = 20
+@export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
@@ -29,5 +31,23 @@ func _physics_process(delta):
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+	
 	velocity = target_velocity
 	move_and_slide()
+	
+	# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		var collision = get_slide_collision(index)
+		
+		if collision.get_collider() == null:
+			continue
+		
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			# Check that we are hitting mob from above
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				break
