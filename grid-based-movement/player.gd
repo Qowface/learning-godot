0b4fbcba6b53b8806bea8a70222e7c53c5af339b
Robert Marquess <qowsrule@gmail.com>
2024-08-11 12:54:50 -0400

    Grid-Based Movement - Add tween to bounce back on collision.

--- grid-based-movement/player.gd
+++ grid-based-movement/player.gd
@@ -9,7 +9,7 @@ var inputs = {
 	"down": Vector2.DOWN
 }
 
-var animation_speed = 3
+var animation_speed = 4
 var moving = false
 
 @onready var ray = $RayCast2D
@@ -20,23 +20,35 @@ func _ready():
 	position += Vector2.ONE * tile_size / 2
 
 
-func _unhandled_input(event):
+func _process(delta):
 	if moving:
 		return
 	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
+		if Input.is_action_pressed(dir):
 			move(dir)
 
 
 func move(dir):
 	ray.target_position = inputs[dir] * tile_size
 	ray.force_raycast_update()
+	
+	moving = true
+	$AnimationPlayer.play(dir)
+	var tween = create_tween()
+	
 	if !ray.is_colliding():
-		#position += inputs[dir] * tile_size
-		var tween = create_tween()
 		tween.tween_property(self, "position",
-			position + inputs[dir] * tile_size, 1.0 / animation_speed).set_trans(Tween.TRANS_SINE)
-		moving = true
-		$AnimationPlayer.play(dir)
-		await tween.finished
-		moving = false
+			position + inputs[dir] * tile_size,
+			1.0 / animation_speed)
+	else:
+		var start_pos = position
+		var end_pos = position + inputs[dir] * (tile_size / 2)
+		tween.tween_property(self, "position",
+			end_pos,
+			0.5 / animation_speed).from(start_pos)
+		tween.tween_property(self, "position",
+			start_pos,
+			0.5 / animation_speed).from(end_pos)
+	
+	await tween.finished
+	moving = false
